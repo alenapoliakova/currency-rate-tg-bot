5322c7b2781b5f46e0bad9d3872edbcdfbfb96d8
alenapoliakova <a.polyackowa2016@gmail.com>
2022-03-12 23:48:51 +0300

    solved issue #1 - added support for entry date

--- bot.py
+++ bot.py
@@ -1,31 +1,65 @@
 from telebot import TeleBot, types
 from requests import get
-from re import findall
+from re import findall, match
 from bs4 import BeautifulSoup as bs
 from time import strftime, localtime
 from settings import BOT_TOKEN, BOT_NAME
+from collections import namedtuple
 
 
 bot = TeleBot(BOT_TOKEN, parse_mode='html')
 name = BOT_NAME
 
+date = namedtuple('Date', ['day', 'month', 'year'])
 
 db = {}
 data = {
     'start': f'Привет! Бот <b>{name}</b> делится с вами курсом валют с <b>сайта ЦБ</b>.\n\n'
              f'Отправьте <b>/currency</b>, чтобы выбрать валюту.',
-    'open_markup': 'Откройте клавиатуру для выбора валюты.',
-    'close_markup': 'Вы закрыли клавиатуру. Отправьте <b>/currency</b>, чтобы открыть клавиатуру.'
+    'open_markup': 'Клавиатура открыта, выберите валюту.',
+    'currency': 'Отправьте мне интересующую вас дату в формате: 02.02.2022, 02 02 2022, 02.02.22 или 02 02 22',
+    'answer_to_user': 'Я не понял вас. Убедитесь, что ваша дата в формате день.месяц.год или день месяц год.',
+    'close_markup': 'Вы закрыли клавиатуру. Отправьте <b>/currency</b>, чтобы открыть клавиатуру.',
+    'not_in_dict': 'Вы ввели команду, которой нет в моём словаре. Отправьте <b>/start</b>, чтобы начать общение с ботом.'
 }
 
 
-def get_currency(actual_currency: str):
+def parse_date(actual_date: str) -> tuple | int:
+    try:
+        real_date = match(r'^[0-9]{1,2}[ .][0-9]{1,2}[ .][0-9]{4}', actual_date).group()
+    except TypeError:
+        return 0
+    except AttributeError:  # Обработка ошибки для .group()
+        return 0
+    else:
+        now_day, now_month, now_year = strftime("%d %m %Y", localtime()).split()
+
+        if '.' in real_date:
+            real_date = real_date.split('.')
+        else:
+            real_date = real_date.split()
+
+        new_date = date(real_date[0], real_date[1], real_date[2])
+
+        if int(new_date.year) > int(now_year):
+            return 0
+        elif int(new_date.year) == int(now_year) and int(new_date.month) > int(now_month):
+            return 0
+        elif int(new_date.year) == int(now_year) and int(new_date.month) == int(now_month) and int(new_date.day) > int(now_day):
+            return 0
+
+        return new_date
+
+
+def get_currency(actual_currency: str, date_to_parse: tuple):
     """
     Функция для парсинга курса ЦБ. Если данный курс уже парсился, то данные возьмутся из словаря db.
     :param actual_currency: Имя валюты.
+    :param date_to_parse: Кортеж, который содержит дату для парсинга.
     :return: Текст с именем валюты и её курсом.
     """
-    day, month, year = strftime("%d %m %Y", localtime()).split()
+    day, month, year = date_to_parse.day, date_to_parse.month, date_to_parse.year
+    # day, month, year = strftime("%d %m %Y", localtime()).split()
 
     currency = db.get(f'{day} {month} {year} {actual_currency}', None)
     if currency:
@@ -68,19 +102,37 @@ def currency(message):
     bot.send_message(message.chat.id, data['open_markup'], reply_markup=markup)
 
 
+def answer_to_user(message, **kwargs):
+    # TODO: распарсить здесь дату и добавить в get_currency выбор даты
+
+    parsed_date = parse_date(actual_date=message.text)
+
+    if isinstance(parsed_date, date):
+        bot.send_message(message.chat.id, get_currency(actual_currency=kwargs['valute'], date_to_parse=parsed_date))
+    else:
+        bot.send_message(message.chat.id, data['answer_to_user'])
+
+
 @bot.message_handler(content_types='text')
 def reply(message):
+
     match message.text:
         case 'Курс Доллара США 💲':
-            bot.send_message(message.chat.id, get_currency('Доллар США'))
+            msg = bot.send_message(message.chat.id, data['currency'])
+            bot.register_next_step_handler(message=msg, callback=answer_to_user, valute='Доллар США')
         case 'Курс Евро 💶':
-            bot.send_message(message.chat.id, get_currency('Евро'))
+            msg = bot.send_message(message.chat.id, data['currency'])
+            bot.register_next_step_handler(message=msg, callback=answer_to_user, valute='Евро')
         case 'Курс Фунта стерлингов 💷':
-            bot.send_message(message.chat.id, get_currency('Фунт стерлингов'))
+            msg = bot.send_message(message.chat.id, data['currency'])
+            bot.register_next_step_handler(message=msg, callback=answer_to_user, valute='Фунт стерлингов')
         case 'Курс Белорусского рубля 🇧🇾':
-            bot.send_message(message.chat.id, get_currency('Белорусский рубль'))
-        case 'Назад':
+            msg = bot.send_message(message.chat.id, data['currency'])
+            bot.register_next_step_handler(message=msg, callback=answer_to_user, valute='Белорусский рубль')
+        case 'Назад':  # Закрытие клавиатуры
             bot.send_message(message.chat.id, data['close_markup'], reply_markup=types.ReplyKeyboardRemove())
+        case _:
+            bot.send_message(message.chat.id, data['not_in_dict'])
 
 
 bot.polling(non_stop=True)
